71c8f12340b83560852bee8dbb232bc26f07058d
Sivasankaran K B <kbsiva7@gmail.com>
2025-09-14 19:40:37 +0530

    Add header guards to mdparser.h

--- src/mdparser.h
+++ src/mdparser.h
@@ -1,3 +1,6 @@
+# ifndef MDPARSER_H
+# define MDPARSER_H
+
 class MdParserPlugin{
     public:
     virtual void addTextElement(const wchar_t *text);
@@ -10,3 +13,5 @@ class MdParser{
     void addPlugin(MdParserPlugin *plugin);
     void parseLine(const wchar_t *source);
 };
+
+# endif
